7a82043415e537e63b4c56097167b0c9a5183188
Alexandr Dem'yanenko <demjanenko@gmail.com>
2016-10-27 03:04:14 +0300

    haxelib dep example

--- src/main/haxe/Main.hx
+++ src/main/haxe/Main.hx
@@ -1,10 +1,11 @@
 import sample.SampleClass;
-
+import motion.MotionPath;
 class Main
 {
 	static function main() : Void
 	{
         var s = new SampleClass();
+        var d = new MotionPath();
 		trace('Hello Gradle World ${s.foo(10)}');
 	}
 }
