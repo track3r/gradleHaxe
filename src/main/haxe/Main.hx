5886b5c6dea23af0f7773ddd1aacd76f20d76021
Alexandr Dem'yanenko <demjanenko@gmail.com>
2016-11-09 00:21:23 +0300

    updated sample to test classPath directive for haxelibs

--- src/main/haxe/Main.hx
+++ src/main/haxe/Main.hx
@@ -1,5 +1,7 @@
 import sample.SampleClass;
 import motion.MotionPath;
+import de.polygonal.ds.Dll;
+
 class Main
 {
 	static function main() : Void
@@ -7,5 +9,9 @@ class Main
         var s = new SampleClass();
         var d = new MotionPath();
 		trace('Hello Gradle World ${s.foo(10)}');
+
+        var list: Dll<String> = new Dll<String>();
+        list.append("list test");
+        trace(list.removeTail());
 	}
 }
